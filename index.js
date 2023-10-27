8408fbb704b6f2c6f2188a3e813102cc9e91c841
Houston Davis <133936381+HDavis147@users.noreply.github.com>
2023-10-26 19:36:21 -0500

    Completed application - shapes render correctly. Removed unused files.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { Triangle, Square, Circle } = require("./lib/shapes.js");
 
 // Function to generate the final SVG and save it. 
 function generateSVG(results) {
@@ -13,9 +14,31 @@ function generateSVG(results) {
     finalShape = new Circle(results);
   } 
 
-  console.log(finalShape);
+  // Converts input text to uppercase and sets the SVG file's name. 
+  const text = results.text.toUpperCase();
+  const fileName = `${text}.svg`;
+
+  // If the shape is a triangle, the text needs to be moved down to fit in the shape.
+  if(results.shape === 'triangle') {
+    var textTag= `<text x="100" y="180" text-anchor="middle" font-size="80" fill="${results.textColor}">${text}</text>`;
+  } else {
+    var textTag= `<text x="100" y="120" text-anchor="middle" font-size="80" fill="${results.textColor}">${text}</text>`;
+  }
+
+  // Sets size of SVG
+  const svgTag = `<svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">`;
+
+  // Combines all parameters to create the final SVG.
+  const completeSVG = svgTag + finalShape.render() + textTag + `</svg>`;
+
+  // Writes SVG to file. 
+  fs.writeFile(fileName, completeSVG, (err) => {
+    if (err) throw err;
+    console.log('Logo has been generated and saved!');
+  });
 }
 
+// Function to prompt the user for input.
 function prompter(){
   inquirer
       .prompt([
@@ -33,7 +56,7 @@ function prompter(){
           type: 'list',
           name: 'shape',
           message: 'Please choose your shape.',
-          choices: ['square', 'triange', 'circle']
+          choices: ['square', 'triangle', 'circle']
         },
         {
           type: 'input',
@@ -42,7 +65,6 @@ function prompter(){
         }
       ])
       .then((results) => {
-        console.log(results);
         // Validation for text length
         if (results.text.length > 3) {
           console.log('Please enter text with 3 characters or less.');
@@ -52,3 +74,6 @@ function prompter(){
         }
       });
 }
+
+// Calls the prompter function to start the application. 
+prompter();
